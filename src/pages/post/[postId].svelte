34d73c7fa74b49b9b66bcb0d7b753c2a6d5f31e3
kwagi <heisenberg123457@gmail.com>
2023-07-09 08:01:45 +0900

    변수 비동기 정리

--- src/pages/post/[postId].svelte
+++ src/pages/post/[postId].svelte
@@ -10,23 +10,17 @@
     let { email } = $myInfo
     let postId = $params.postId
     let replyId = 0
+
     let requestURL = `http://${$host}/api/post/${postId}`
-    let post = {
-        title: "",
-        contents: "",
-        poster: "",
-        postDate: "",
-        likes: "",
-        hits: "",
-    }
-    let images = []
-    let reply = []
-    axios.get(requestURL).then((res) => {
+    const posting = axios.get(requestURL).then((res) => {
         console.log(res.data)
         post = res.data.post
         images = res.data.images
         reply = res.data.reply
+        return { post, images, reply }
     })
+    let { post, images, reply } = posting
+
     const clickLikes = () => {
         let requestURL = `http://${$host}/api/post/click-likes/${postId}`
         let token = localStorage.getItem("token")
@@ -88,89 +82,90 @@
     bind:isDeleteReply="{isDeleteReply}"
     bind:postId="{postId}"
     bind:replyId="{replyId}" />
-
-<ul class="list-group">
-    <li class="list-group-item">제목: {post.title}</li>
-    <li class="list-group-item">
-        작성일: {post.postDate}
-    </li>
-</ul>
-
-<div class="text-center p-5">
-    {#each images as image}
-        <div class="main">
-            {#if image === ""}
-                <img
-                    src="https://c.tenor.com/On7kvXhzml4AAAAi/loading-gif.gif"
-                    alt="Loading" />
-            {:else}
-                <img src="{dataFormat}{image}" alt="" />
-            {/if}
-        </div>
-    {/each}
-</div>
-<div class="text-center p-5 text-break">
-    <h5>{post.contents}</h5>
-</div>
-
-<div class="text-center" style="padding: 10em">
-    {#if $isLogin === true}
-        <button class="btn btn-outline-dark" on:click="{clickLikes}"
-            >추천: {post.likes}</button>
-        {#if email === post.poster}
-            <button
-                on:click="{() => {
-                    showModal = true
-                    isDeleteMyPost = true
-                }}"
-                class="btn btn-outline-danger">글 삭제</button>
-        {/if}
-    {/if}
-</div>
-<table class="type09 text-dark">
-    <thead>
-        <tr>
-            <th>작성자</th>
-            <th>내용</th>
-            <th>작성일</th>
-            {#if $isLogin === true}
-                <th>삭제</th>
-            {/if}
-        </tr>
-    </thead>
-
-    <tbody>
-        {#each reply as { writer, replyDate, replyContents, postReplyStatus, replyId: myReplyId }}
-            {#if postReplyStatus === "ALL"}
-                <tr>
-                    {#if writer === email}
-                        <td class="text-break">나</td>
-                    {:else}
-                        <td class="text-break">{writer}</td>
-                    {/if}
-                    <td class="text-break">{replyContents}</td>
-                    <td>{replyDate}</td>
-                    <td
-                        ><button
-                            on:click="{() => {
-                                showModal = true
-                                isDeleteReply = true
-                                replyId = myReplyId
-                            }}"
-                            class="btn btn-outline-danger">삭제</button
-                        ></td>
-                </tr>
-            {:else}
-                <td>삭제된 댓글입니다.</td>
-            {/if}
-        {/each}
-    </tbody>
-</table>
-
-{#if $isLogin === true}
-    <div class="text-center">
-        <textarea class="col-6" bind:value="{replyContents}"></textarea>
-        <button on:click="{writeReply}" class="btn btn-outline-primary"
-            >작성</button>
+{#await posting}
+    <div class="main">
+        <img
+            src="https://c.tenor.com/On7kvXhzml4AAAAi/loading-gif.gif"
+            alt="Loading" />
     </div>
-{/if}
+{:then { post, images, reply }}
+    <ul class="list-group">
+        <li class="list-group-item">제목: {post.title}</li>
+        <li class="list-group-item">
+            작성일: {post.postDate}
+        </li>
+    </ul>
+
+    <div class="text-center p-5">
+        {#each images as image}
+            <div class="main">
+                <img src="{dataFormat}{image}" alt="" />
+            </div>
+        {/each}
+    </div>
+    <div class="text-center p-5 text-break">
+        <h5>{post.contents}</h5>
+    </div>
+
+    <div class="text-center" style="padding: 10em">
+        {#if $isLogin === true}
+            <button class="btn btn-outline-dark" on:click="{clickLikes}"
+                >추천: {post.likes}</button>
+            {#if email === post.poster}
+                <button
+                    on:click="{() => {
+                        showModal = true
+                        isDeleteMyPost = true
+                    }}"
+                    class="btn btn-outline-danger">글 삭제</button>
+            {/if}
+        {/if}
+    </div>
+    <table class="type09 text-dark">
+        <thead>
+            <tr>
+                <th>작성자</th>
+                <th>내용</th>
+                <th>작성일</th>
+                {#if $isLogin === true}
+                    <th>삭제</th>
+                {/if}
+            </tr>
+        </thead>
+
+        <tbody>
+            {#each reply as { writer, replyDate, replyContents, postReplyStatus, replyId: myReplyId }}
+                {#if postReplyStatus === "ALL"}
+                    <tr>
+                        {#if writer === email}
+                            <td class="text-break">나</td>
+                        {:else}
+                            <td class="text-break">{writer}</td>
+                        {/if}
+                        <td class="text-break">{replyContents}</td>
+                        <td>{replyDate}</td>
+                        <td
+                            ><button
+                                on:click="{() => {
+                                    showModal = true
+                                    isDeleteReply = true
+                                    replyId = myReplyId
+                                }}"
+                                class="btn btn-outline-danger">삭제</button
+                            ></td>
+                    </tr>
+                {:else}
+                    <td>삭제된 댓글입니다.</td>
+                {/if}
+            {/each}
+        </tbody>
+    </table>
+
+    {#if $isLogin === true}
+        <div class="text-center">
+            <textarea class="col-6" bind:value="{replyContents}"></textarea>
+            <button on:click="{writeReply}" class="btn btn-outline-primary"
+                >작성</button>
+        </div>
+    {/if}
+{/await}
